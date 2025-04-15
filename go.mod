f2b98d36273e7cc335e817d7c42318aefab2051c
Denis Lavrinov <da.lavrinov@yandex.ru>
2025-04-15 17:24:07 +0300

    add config

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module app
 
 go 1.23.2
+
+require github.com/kelseyhightower/envconfig v1.4.0 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,24 @@
 package config
+
+import (
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	HTTPport   string `envconfig:"API_PORT"`
+	DBhost     string `envconfig:"DB_HOST"`
+	DBname     string `envconfig:"POSTGRES_DB"`
+	DBusername string `envconfig:"POSTGRES_USER"`
+	DBpassword string `envconfig:"POSTGRES_PASSWORD"`
+}
+
+func GetConfig() (*Config, error) {
+	cfg := &Config{}
+	err := envconfig.Process("", cfg)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
