22c37a88eb15e87f89aa18e448128ad1cf6c8f40
Denis Lavrinov <da.lavrinov@yandex.ru>
2025-04-15 22:08:16 +0300

    update cfg

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,11 +5,14 @@ import (
 )
 
 type config struct {
-	HTTPport   string `envconfig:"API_PORT"`
-	DBhost     string `envconfig:"DB_HOST"`
-	DBname     string `envconfig:"POSTGRES_DB"`
-	DBusername string `envconfig:"POSTGRES_USER"`
-	DBpassword string `envconfig:"POSTGRES_PASSWORD"`
+	HTTPport               string `envconfig:"API_PORT"`
+	DBhost                 string `envconfig:"DB_HOST"`
+	DBname                 string `envconfig:"POSTGRES_DB"`
+	DBusername             string `envconfig:"POSTGRES_USER"`
+	DBpassword             string `envconfig:"POSTGRES_PASSWORD"`
+	AccessTokenKey         string `envconfig:"ACCESS_KEY"`
+	AccessTokenExpMinutes  int    `envconfig:"ACCESS_EXP_MINUTES"`
+	RefreshTokenExpMinutes int    `envconfig:"REFRESH_EXP_MINUTES"`
 }
 
 func GetConfig() (*config, error) {
